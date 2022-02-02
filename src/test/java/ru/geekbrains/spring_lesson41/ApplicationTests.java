18c767c0ba74ad0233df4c8ea06c0c0c81aeaba1
Viktor Khalezov <viktor-khalezov@mail.ru>
2022-02-02 21:14:56 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/spring_lesson41/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.spring_lesson41;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
